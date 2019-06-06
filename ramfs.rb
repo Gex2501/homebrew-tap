779d16b8f289312d3a1091f909324d0c6f669967
Gregory D. Estes <greg.estes@gmail.com>
2019-06-06 03:07:03 -0700

    Fix test condition.

--- ramfs.rb
+++ ramfs.rb
@@ -13,12 +13,12 @@ class Ramfs < Formula
 
   test do
     # test script output
-    assert_equal %x('#{bin}/ramfs'), "ramfs  --unmount <path>
+    assert_match %x('#{bin}/ramfs'), "ramfs  --unmount <path>
 ramfs  --size <size> [--name <label>] [--filesystem <format>] [--mount <path>]
       [--allow-suid] [--enable-owners]
         'size' may be expressed in IEC units, e.g. 512M, 2G, etc.
         'path' will be created if it does not exist, but should be empty
-      To view the available filesystem formats, use 'format' \"help\""
+      To view the available filesystem formats, use 'format' .help."
   end
 
 end
