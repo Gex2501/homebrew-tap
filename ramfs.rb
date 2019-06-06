c519173a973d6ddd1e2ee7abe45800fc76da7bed
Gregory D. Estes <greg.estes@gmail.com>
2019-06-06 02:12:34 -0700

    Updated Formulae

--- ramfs.rb
+++ ramfs.rb
@@ -2,7 +2,7 @@ class Ramfs < Formula
   desc "A memory-backed filesystem mounter for Mac OS X"
   homepage "https://github.com/Gex2501/ramfs"
   url "https://github.com/Gex2501/ramfs/raw/master/ramfs-1.0.tar.gz"
-  sha256 "ce3028a7851307a8088871d0d2f3b7de61464873bd3ec565b30bbc078a47723e"
+  sha256 "a20c92b5842d8ddbf55a419f903aac0d7a6013fb11fee2dffb250068e017d935"
 
   def install
     # install ramfs script, create symlink to script in /usr/local/bin
