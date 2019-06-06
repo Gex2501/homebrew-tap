fdcdae010ee4ea59d9784e1ddd15d4ab00111413
Gregory D. Estes <greg.estes@gmail.com>
2019-06-06 02:21:46 -0700

    Added coreutils dependency.

--- ramfs.rb
+++ ramfs.rb
@@ -4,6 +4,8 @@ class Ramfs < Formula
   url "https://github.com/Gex2501/ramfs/raw/master/ramfs-1.0.tar.gz"
   sha256 "a20c92b5842d8ddbf55a419f903aac0d7a6013fb11fee2dffb250068e017d935"
 
+  depends_on "coreutils"
+
   def install
     # install ramfs script, create symlink to script in /usr/local/bin
     bin.install "ramfs"
